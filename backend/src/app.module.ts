b204829cd3a6c198a79964038d4d59bdbc7c0e3a
muhtegaralfikri <105841110722@student.unismuh.ac.id>
2025-11-02 00:40:51 +0800

    feat passport jwt

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // <-- 2. Tambahkan di sini
+    PrismaModule,
+    AuthModule, // <-- 3. Kita juga akan butuh ini (tambahkan jika belum ada)
+    UsersModule, // <-- 4. Dan ini (tambahkan jika belum ada)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module'; // <-- 1. Import UsersModule
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    UsersModule, // <-- 2. Tambahkan di sini
+    PassportModule,
+    JwtModule.registerAsync({
+      // <-- 3. Konfigurasi JwtModule secara dinamis
+      imports: [ConfigModule], // Impor ConfigModule
+      inject: [ConfigService], // Inject ConfigService
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'), // Baca dari .env
+        signOptions: { expiresIn: '1d' }, // Token berlaku 1 hari
+      }),
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
