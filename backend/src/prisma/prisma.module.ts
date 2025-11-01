5721fea805ee36a085e6085b9bd04f32918332d5
muhtegaralfikri <105841110722@student.unismuh.ac.id>
2025-11-02 00:21:34 +0800

    feat jwt and other

--- backend/src/prisma/prisma.module.ts
+++ backend/src/prisma/prisma.module.ts
@@ -1,7 +1,10 @@
+// src/prisma/prisma.module.ts
+
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  providers: [PrismaService]
+  providers: [PrismaService], // Daftarkan service-nya
+  exports: [PrismaService], // Ekspor service-nya agar bisa dipakai modul lain
 })
 export class PrismaModule {}
