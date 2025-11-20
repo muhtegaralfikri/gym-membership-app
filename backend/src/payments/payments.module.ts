604e4851b60393258f32b259007c34da547ae679
muhtegaralfikri <105841110722@student.unismuh.ac.id>
2025-11-20 14:02:26 +0800

    feat Tambah scheduler pembersihan pending

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -3,10 +3,11 @@ import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { MembershipsModule } from 'src/memberships/memberships.module';
+import { PaymentsCleanupService } from './payments.cleanup.service';
 
 @Module({
   imports: [PrismaModule, MembershipsModule], // <-- 2. Tambahkan di sini
   controllers: [PaymentsController],
-  providers: [PaymentsService],
+  providers: [PaymentsService, PaymentsCleanupService],
 })
 export class PaymentsModule {}
